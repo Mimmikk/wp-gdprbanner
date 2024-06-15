e36c1bdcee68a4ae20acc6ee0afbac05df12c5d3
Eivind <10182309+cnoid@users.noreply.github.com>
2024-06-15 23:03:06 +0200

    Create banner.php

---
+++ lib/banner.php
@@ -0,0 +1,14 @@
+<div class="gdpr-cookie-consent">
+    <div class="cookie-text">
+        <p style="font-size: 0.9em; margin-bottom: 0px;">We use technologies such as Cookies and Analytics to optimize our content delivery and enable functionality.</p>
+        <p style="font-size: 0.8em; margin-bottom: 0px;">By clicking accept, you consent to the use of the technologies we utilize. You may also change the settings according to your preferences.</p>
+    </div>
+    <div class="cookie-buttons">
+        <button type="button" class="accept-cookies" onclick="updateConsentGranted()">Accept</button>
+        <button type="button" class="dismiss-cookies" onclick="updateConsentDenied()">Decline</button>
+        <button type="button" class="preferences-cookies">Preferences</button>
+    </div>
+    <div>
+        <a href="/privacy-policy" class="policies">Privacy Policy</a> | <a href="/terms" class="policies">Terms and Conditions</a>
+    </div>
+</div>
